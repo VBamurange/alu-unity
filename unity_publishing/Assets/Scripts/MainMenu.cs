8b8ec190a21aaa1a0f8b3a634ff6c6953250e161
Victoria Bamurange <v.bamurange@alustudent.com>
2024-05-25 15:15:39 +0200

    changes

--- unity_publishing/Assets/Scripts/MainMenu.cs
+++ unity_publishing/Assets/Scripts/MainMenu.cs
@@ -4,25 +4,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Material trapMat;
     public Material goalMat;
-    public colorblindMode;
+    public Toggle colorblindMode;
 
     public void PlayMaze()
     {
-        if (colorblindMode.isOn)
+        trapMat.color = new Color32(255, 0, 0, 255);
+        goalMat.color = new Color32(0, 255, 0, 255);
+      
+        if (colorblindMode !=null && colorblindMode.isOn)
         {
-            trapMat.color = new Color32(255, 112, 0, 1);
+            trapMat.color = new Color32(255, 112, 0, 255);
             goalMat.color = Color.blue;
         }
-        SceneManager.LoadScene("MazeScene");
+        SceneManager.LoadScene("Maze");
     }
     public void QuitMaze()
     {
-        Debug.Log("Quit Game");
+        UnityEngine.Debug.Log("Quit Game");
         Application.Quit();
     }
 }
