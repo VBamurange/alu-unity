23d8aeaa1351e78fcd6adcc5c45585cde23a23df
Victoria Bamurange <v.bamurange@alustudent.com>
2024-06-04 12:17:44 +0200

    fourth commit

--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -8,40 +6,67 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+    private Transform cameraPosition;
     private Rigidbody rb;
-    private bool isGrounded;
+    private Vector3 vertical;
+    private Vector3 horizontaL;
+    private float moveX;
+    private float moveZ;
+    private Vector3 InitialPosition;
+    public CharacterController controller;
 
+    private void Awake()
+    {
+        InitialPosition = transform.position;
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        if (cameraPosition == null)
+        {
+            cameraPosition =   Camera.main.transform;
+        }
     }
 
     void Update()
     {
         Move();
         Jump();
+        CheckFall();
     }
 
     void Move()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+         moveX = Input.GetAxis("Horizontal");
+         moveZ = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        rb.MovePosition(transform.position + move * moveSpeed * Time.deltaTime);
+        Vector3 forward = cameraPosition.forward;
+        Vector3 right = cameraPosition.right;
+
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+        rb.AddForce(move * (Time.deltaTime * moveSpeed));
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
+            rb.AddForce(Vector3.up * jumpForce);
         }
     }
 
-    void OnCollisionStay(Collision collision)
+    void CheckFall()
     {
-        isGrounded = true;
+        Vector3 spaceGap = new Vector3(0, 3, 0);
+        if (transform.position.y < -3f)
+        {
+            transform.position = InitialPosition + spaceGap;
+        }
     }
 }
